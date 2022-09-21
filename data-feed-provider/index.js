90e4356f7b20309180535fd5f9ccfdbceeb7a225
Jose Manuel Cantera <jose.cantera@iota.org>
2022-09-21 12:12:09 +0200

    Data Feed Provider refinement

--- data-feed-provider/index.js
+++ data-feed-provider/index.js
@@ -1,19 +1,47 @@
 import Fastify from "fastify";
 import fetch, { Headers } from "node-fetch";
 
+// Needed as the original service seems to be behind a self-signed certificate
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
 const SERVICE_ENDPOINT = "https://tlip.cmacloudservices.com";
 const SERVICE_ROUTE = "/cma-services/rest/events/tlip";
 
+// Template of the data feed as schema.org
 const resultDataFeedTemplate = {
-  "@context": ["https://schema.org"],
+  "@context": ["https://schema.org", "https://www.w3.org/ns/activitystreams"],
   type: "DataFeed",
   dataFeedElement: [],
 };
 
+function convertLocationToDecimal(event) {
+    const latitude = event.latDegree + event.latMinute / 60;
+    if (event.latHemisphere === "S") {
+        latitude*=-1;
+    }
+
+    const longitude = event.longDegree + event.longMinute / 60;
+    if (event.longHemishere === "W") {
+        longitude *= -1;
+    }
+    
+    return { latitude, longitude };
+}
+
+function translateEventType(event) {
+    switch(event.eventType) {
+        case "CHECKPOINT_ARRIVAL":
+            return "ArrivalAction";
+        case "LAT_ENTERED_ZONE":
+            return "Place";
+    }
+}
+
 function buildDataFeedItem(event) {
   return {
     item: {
-      type: event.eventType,
+      type: translateEventType(event),
+      ...convertLocationToDecimal(event)
     },
     dateModified: event.eventDate,
   };
@@ -22,18 +50,25 @@ function buildDataFeedItem(event) {
 const getDataFeed = async (request, reply) => {
   // The Authorization header is just propagated to the service
   const authorization = request.headers["authorization"];
-
   const identifier = request.query["identifier"];
 
+  if (!identifier) {
+    throw new Error("Identifier query param not supplied");
+  }
+
+  // Authorization credentials is just propagated
   const headers = new Headers();
   headers.append("Authorization", authorization);
 
   const url = new URL(`${SERVICE_ENDPOINT}${SERVICE_ROUTE}`);
   const params = url.searchParams;
 
+  // Hardcoded values that should be tuned later
   params.append("createdDateFrom", "2022-08-02T10:23:50Z");
   params.append("createdDateTo", "2022-08-02T10:33:50Z");
-  params.append("UCR", identifier);
+
+  const elements = identifier.split(":");
+  params.append("UCR", elements[elements.length - 1]);
 
   const serviceResponse = await fetch(url, { headers });
 
@@ -64,8 +99,6 @@ const fastify = Fastify({
 });
 
 fastify.get("/", getDataFeed);
-
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 /**
  * Run the server!
